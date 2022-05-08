fe26d55f12346dbf6a4e7e6ab598d0c3255fc37c
arcanedisgea <39720490+ArcaneDisgea@users.noreply.github.com>
2022-05-08 14:14:05 -0400

    duplicate additional info

--- commands/feature-request.js
+++ commands/feature-request.js
@@ -10,7 +10,6 @@ module.exports = {
       .addField("Describe the solution you'd like.", "A clear and concise description of what you want to happen.")
       .addField("Describe alternatives you've considered.", "A clear and concise description of any alternative solutions or features you've considered.")
       .addField("Additional context", "Add any other context or screenshots about the feature request here.")
-      .addField("Additional Information", "Any additional context can help diagnose the issue. The more info the better!")
       .addField(
         "Feature Request Template",
         "```**Is your feature request related to a problem? Please describe.**\n> \n**Describe the solution you'd like**\n> \n**Describe alternatives you've considered**\n> \n**Additional context**\n> \n\nCopy this and fill next to the >```"
