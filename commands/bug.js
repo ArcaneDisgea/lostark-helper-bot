6258ff910702baa39360b1180e5fc520c4fb9aa1
arcanedisgea <39720490+ArcaneDisgea@users.noreply.github.com>
2022-05-08 13:52:33 -0400

    bug command

---
+++ commands/bug.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("bug").setDescription("Get information on how to draft a bug report."),
+  async execute(interaction) {
+    const consoleEmbed = new MessageEmbed()
+      .setTitle("How to submit a bug report for Lost Ark Helper")
+      .setDescription("A clear and concise description of what the bug is.")
+      .addField("Steps to reproduce!", "A bug that can't be reproduced can't be fixed. Explain in detail what needs to be done to create the bug you are experiencing.")
+      .addField("Expected Behavior!", "A clear and concise description of what you expected to happen.")
+      .addField(
+        "Screenshots!",
+        "Please provide any screenshots you can of what is happening. Opening the console via CTRL + SHIFT + I and clicking the console tab can also help us diagnose the issue."
+      )
+      .addField("Software Version/Type!", "Are you using the website or the desktop app? If the app then what version? If the website then what browser?")
+      .addField("Additional Information", "Any additional context can help diagnose the issue. The more info the better!")
+      .addField(
+        "Bug Template",
+        "```**Issue:** \n> \n**Steps to Reproduce:**\n> \n**Expected Behaviour:**\n> \n**Teamcraft Version - Browser / Desktop Client**\n> \n**Additional Information:**\n> \n**Screenshots:**\n\nCopy this and fill next to the > ```"
+      );
+    return interaction.reply({ embeds: [consoleEmbed] });
+  },
+};
